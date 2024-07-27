3a7e660e64e4743f6f71e31057d2e877c0fd597c
hemehzhao <18137930317@163.com>
2024-07-27 15:04:01 +0800

    fix: 修改打包输出文件

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,10 +9,6 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
-    },
-    build: {
-      outDir: 'docs'
     }
-  }),
-
+  })
 )
