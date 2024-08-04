261a190b93a6835b9636e8837e2e2040634d98a9
hemehzhao <18137930317@163.com>
2024-08-04 20:27:45 +0800

    feature(custom):

--- src/views/css/index.vue
+++ src/views/css/index.vue
@@ -2,9 +2,11 @@
   <n-card title="歌曲" style="margin-bottom: 16px">
     <n-tabs type="line" animated>
       <n-tab-pane name="attribute" tab="css属性">
-        <div>
-          <div>边框颜色跟随文字颜色一致</div>
-          <div>currentColor</div>
+        <div class="currentColor">
+          <div>
+            <n-color-picker v-model:value="color" />
+          </div>
+          <div class="box">边框颜色跟随文字颜色一致</div>
         </div>
       </n-tab-pane>
       <n-tab-pane name="infiniteBanner" tab="无视差滚动">
@@ -22,6 +24,22 @@
 <script setup lang="ts">
 import infiniteBanner from './compon/infiniteBanner.vue'
 const cunts = ref<number>(0)
+const color = ref()
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.currentColor {
+  width: 200px;
+  padding: 10px;
+  border: 1px solid #333;
+  border-radius: 4px;
+  .box {
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    color: v-bind(color);
+    border: 1px solid currentColor;
+    border-radius: 4px;
+  }
+}
+</style>
